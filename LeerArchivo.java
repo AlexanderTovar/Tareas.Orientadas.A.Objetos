1e80945aa2d1c03fee231239b0e59f158773a86b
AlexanderTovar <118315092+AlexanderTovar@users.noreply.github.com>
2023-05-05 17:28:52 -0600

    Manejo de excepciones 2
    Manejo de excepciones 2

--- LeerArchivo.java
+++ LeerArchivo.java
@@ -1,16 +1,26 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class LeerArchivo {
-    public static void main(String[] args) {
-        String ruta = "Datos.txt";
-        try {
-            BufferedReader lector = new BufferedReader(new FileReader(ruta));
-
-        }catch (FileNotFoundException fnfe){
-            System.out.println("El archivo no existe!");
-            System.out.println(fnfe.getMessage());
+    public void leer(String ruta) throws IOException {
+        //  try {
+        BufferedReader reader = new BufferedReader(new FileReader(ruta));
+        String line = " ";
+        while ((line = reader.readLine()) != null) {
+            System.out.println("Info: " + line);
         }
+
+       /* }catch (IOException e){
+            e.printStackTrace();
+        }*/
+    }
+
+    public void metodoUno(String ruta) throws IOException {
+        this.metodoDos(ruta);
+    }
+
+    public void metodoDos(String ruta) throws IOException {
+        this.leer(ruta);
     }
 }
--- Main.java
+++ Main.java
@@ -1,24 +1,16 @@
-import java.util.Scanner;
+import javax.imageio.IIOException;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Scanner scanner = new Scanner(System.in);
-
-
-        try{
-            int a = 10;
-            int b = Integer.parseInt(scanner.nextLine());
-            // int b = scanner.nextInt();
-            System.out.println("Division:" + (a/b) );
-        } catch (ArithmeticException e) {
-            //e.printStackTrace();
-            System.out.println("No se puede dividir entre 0");
-        } catch (NumberFormatException nfe) {
-            System.out.println("No capturaste el dato correctamente");
-        }finally {
-            System.out.println("siempre se ejecuta...");
+        LeerArchivo lectura = new LeerArchivo();
+        try {
+            lectura.metodoUno("datos.txt");
+        } catch (IOException e){
+            e.printStackTrace();
         }
-        System.out.println("Fin del programa!");
     }
+
+
 }
