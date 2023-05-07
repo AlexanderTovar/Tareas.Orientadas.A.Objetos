7d6d01fb9e7c05290a567c2d040c092b18498b5d
AlexanderTovar <118315092+AlexanderTovar@users.noreply.github.com>
2023-05-07 12:37:14 -0600

    Actividad 13
    Actividad 13

---
+++ Maiin.java
@@ -0,0 +1,7 @@
+public class Maiin {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        VentanaSwing ventanaSwing = new VentanaSwing("Centrigrados a Farenheid");
+
+    }
+}
