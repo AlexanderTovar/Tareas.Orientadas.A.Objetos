ae45165e9eee0536cce881613d351fcbdc0aae06
AlexanderTovar <118315092+AlexanderTovar@users.noreply.github.com>
2023-05-07 12:28:44 -0600

    Tarea Extra 3
    Tarea Extra 3

---
+++ Mainn.java
@@ -0,0 +1,10 @@
+
+
+public class Mainn {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        //Ventana ventana = new Ventana("Layouts");
+        VentanaGrid ventanaGrid = new VentanaGrid("Ventana Layot cuadricula");
+    }
+}
